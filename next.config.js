a3fea6f611e2d11d9d7951c5b53c62017ceae367
Anders Mac Mini <hi@wispy.co>
2021-07-16 20:41:24 -0600

    working now with callback redirect stuff from example repo yay

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/magic-verify",
-        destination: "/profile",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/magic-verify",
+  //       destination: "/profile",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
--- pages/profile.js
+++ pages/profile.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { gql, useQuery } from "@apollo/client";
 import useSWR from "swr";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import { useRouter } from "next/router";
+import { magicClient } from "../lib/magic";
+import axios from "axios";
 
 export const GET_DAD_HAT = gql`
   query FindUserByID($id: ID!) {
@@ -25,32 +27,59 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 function useAuth() {
   const { data: user, error, mutate } = useSWR("/api/user", fetcher);
 
-  const loading = user?.token === false || user === undefined;
+  // const loading = user?.token === false || user === undefined;
 
   return {
     user,
-    loading,
     error,
   };
 }
 
 export default function Profile() {
-  const { user, loading } = useAuth();
-
   const router = useRouter();
+  const { user, loading } = useAuth();
+  const [userState, setUserState] = useState(null);
 
-  // useEffect(() => {
-  //   console.log("window", window);
-  //   console.log(Object.keys(router.query)[0]);
-  //   if (Object.keys(router.query)[0] === "magic_credentials") {
-  //     router.push(profile);
-  //     // window.history.pushState({}, document.title, "/" + "profile");
-  //   }
-  // }, []);
+  const [magicState, setMagicState] = useState(false);
+
+  // if (router.query.magic_credential) {
+
+  // } else {
+
+  useEffect(async () => {
+    console.log("router query", router.query);
+
+    // if (JSON.stringify(router.query) === "{}") {
+    // } else {
+    axios
+      .get("/api/user")
+      .then(function (response) {
+        // handle success
+        console.log("response", response);
+        setUserState(response.data);
+        return response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+    // }
+  }, []);
 
   return (
     <Layout>
-      <main>{loading ? "Loading..." : <Data user={user} />}</main>
+      <main>
+        {userState === null ||
+        userState === undefined ||
+        userState.token === false ? (
+          "Loading..."
+        ) : (
+          <Data user={userState} />
+        )}
+      </main>
     </Layout>
   );
 }
