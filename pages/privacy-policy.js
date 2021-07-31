dd021e8da928d4493709881cd839979ea1083821
Anders macbook Pro <hi@wispy.co>
2021-07-31 15:49:29 -0600

    fixes

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -1,5 +1,5 @@
-import styled from styled-components;
-import Layout from ../components/layout;
+import styled from "styled-components";
+import Layout from "../components/layout";
 
 export default function PrivacyPolicy() {
   return (
