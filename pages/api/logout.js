0e55645d38ba498150f50e995bd5e86c01773d85
Anders Mac Mini <hi@wispy.co>
2021-08-01 15:14:37 -0600

    work

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -106,7 +106,7 @@ export default async (req, res) => {
 
   res.setHeader(
     "Set-Cookie",
-    "fauna_client=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    "fauna_client=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=conundrum-quest.vercel.app"
   );
 
   /* remove cookies from request header */
