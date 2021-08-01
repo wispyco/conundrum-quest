b0e7415936faccb6cab977957384697995624a9f
Anders Mac Mini <hi@wispy.co>
2021-08-01 11:56:55 -0600

    trying try catch error handling

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,11 +4,11 @@ import { createHandlers } from "../../lib/rest-utils";
 const handlers = {
   GET: async (req, res) => {
     console.log("test");
-    //await removeSession(res);
-    res.setHeader(
-      "Set-Cookie",
-      "key=fauna_client; Max-Age=-1; domain=conundrum-quest.vercel.app; path=/profile;expires=Thu, 01 Jan 1970 00:00:01 GMT;"
-    );
+    await removeSession(res);
+    // res.setHeader(
+    //   "Set-Cookie",
+    //   "key=fauna_client; Max-Age=-1; domain=conundrum-quest.vercel.app; path=/profile;expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+    // );
     res.status(200).send({ done: true });
   },
 };
