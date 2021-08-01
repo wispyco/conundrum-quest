2525a875500705b52038cb4a6b222f08cdfb2b8d
Anders Mac Mini <hi@wispy.co>
2021-08-01 13:24:44 -0600

    what

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -83,7 +83,7 @@ export default async (req, res) => {
   res.setHeader("Set-Cookie", [
     serialize("fauna_client", "", {
       maxAge: -1,
-      expires: d.toGMTString(),
+      expires: -1,
       domain: "conundrum-quest.vercel.app",
       httpOnly: true,
       secure: true,
