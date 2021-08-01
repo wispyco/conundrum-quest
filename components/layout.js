61dd9e13eb72f37ef192ee2485ce51c47cf71ac7
Anders Mac Mini <hi@wispy.co>
2021-08-01 14:07:58 -0600

    test

--- components/layout.js
+++ components/layout.js
@@ -17,6 +17,7 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { GrClose } from "react-icons/gr";
 import LogRocket from "logrocket";
 import { Waitlist } from "waitlistapi";
+import Cookies from "js-cookie";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
@@ -26,8 +27,13 @@ export default function Layout({ children }) {
   const logout = () => {
     magicClient.user.logout().then(async (test) => {
       // console.log(await magicClient.user.isLoggedIn()); // => `false`
-      const res = await fetch("/api/logout", {
-        method: "GET",
+      // const res = await fetch("/api/logout", {
+      //   method: "GET",
+      // });
+
+      Cookies.remove("fauna_client", {
+        path: "/",
+        domain: "conundrum-quest.vercel.app",
       });
 
       // document.cookie =
